47a44727930cacd72825847397e1def3412abfa8
SHGroup <sunghun7511@naver.com>
2017-07-26 11:55:28 +0900

    Learn how to use stack in asm

---
+++ study/stackcallret.asm
@@ -0,0 +1,21 @@
+section .data
+	msg db 'FUNC!', 10
+	msg_len equ $-msg
+
+section .text
+	global _start
+
+_start:
+	call Func
+
+	mov eax, 1
+	mov ebx, 0
+	int 80h
+
+Func:
+	mov eax, 4
+	mov ebx, 1
+	mov ecx, msg
+	mov edx, msg_len
+	int 80h
+	ret
